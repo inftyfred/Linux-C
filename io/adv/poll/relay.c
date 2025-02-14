20cdba2fbaf755902eac65f9093543c1fdc56e11
inftyfred <2019107893@qq.com>
2025-02-14 22:28:08 +0800

    revise

--- io/adv/poll/relay.c
+++ io/adv/poll/relay.c
@@ -6,6 +6,7 @@
 #include <fcntl.h>
 #include <errno.h>
 #include <sys/select.h>
+#include <sys/poll.h>
 
 #define TTY1 "/dev/tty11"
 #define TTY2 "/dev/tty12"
@@ -102,7 +103,8 @@ static void relay(int fd1, int fd2)
 {
     int fd1_save, fd2_save;
     struct fsm_st fsm12, fsm21;
-    fd_set rset, wset;
+    struct pollfd pfd[2];
+    nfds_t nfd = 2;
 
     fd1_save = fcntl(fd1,F_GETFL);//获取fd1的access mode
     fcntl(fd1, F_SETFL, fd1_save|O_NONBLOCK);//设置非阻塞模式
@@ -118,46 +120,49 @@ static void relay(int fd1, int fd2)
     fsm21.sfd = fd2;
     fsm21.dfd = fd1;
 
+    pfd[0].fd = fd1;
+    pfd[1].fd = fd2;
+
     while (fsm12.state != STATE_T || fsm21.state != STATE_T)
     {
         //布置监视任务
-        FD_ZERO(&rset);
-        FD_ZERO(&wset);
+        pfd[0].events = 0; //清空事件
+        pfd[1].events = 0;
 
         if(fsm12.state == STATE_R)
         {
-            FD_SET(fsm12.sfd, &rset);
+            pfd[0].events |= POLLIN; //1可读
         }
-        else if(fsm12.state == STATE_W)
+        if(fsm21.state == STATE_W)
         {
-            FD_SET(fsm12.dfd, &wset);
+            pfd[0].events |= POLLOUT; //1可写
         }
-
+        if(fsm12.state == STATE_W)
+        {
+            pfd[1].events |= POLLOUT; //2可写
+        }
         if(fsm21.state == STATE_R)
         {
-            FD_SET(fsm21.sfd, &rset);
+            pfd[1].events |= POLLIN; //2可读
         }
-        else if(fsm21.state == STATE_W)
-        {
-            FD_SET(fsm21.dfd, &wset);
-        }
+        
         //监视
         if(fsm12.state < STATE_AUTO || fsm21.state < STATE_AUTO)
         {
-            if(select(max(fd1, fd2)+1, &rset, &wset, NULL, NULL) < 0)
+            while(poll(pfd, nfd, -1) < 0)
             {
                 if(errno == EINTR)
                     continue;
-                perror("select");
+                perror("poll()");
                 exit(1);
             }
         }
         //查看监视结果
         //根据监视结果 推动状态机
         //当大于AUTO状态时，我们也无条件推状态机
-        if(FD_ISSET(fd1, &rset) || FD_ISSET(fd2, &wset) || fsm12.state > STATE_AUTO)
+        if(pfd[0].revents & POLLIN || pfd[1].revents & POLLOUT || fsm12.state > STATE_AUTO)
             fsm_driver(&fsm12);
-        if(FD_ISSET(fd2, &rset) || FD_ISSET(fd1, &wset) || fsm21.state > STATE_AUTO)
+        if(pfd[1].revents & POLLIN || pfd[0].revents & POLLOUT || fsm21.state > STATE_AUTO)
             fsm_driver(&fsm21);
     }
 
